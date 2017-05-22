fecd55f265d5dfe05dc7c04e8f1c43217b1e476d
Ben Dechrai <ben@dechrai.com>
2017-05-22 17:43:27 +1000

    Create accounts files

---
+++ transaction.dev/app/Http/Controllers/AccountController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Account;
+use Illuminate\Http\Request;
+
+class AccountController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return Account::create[$request->all()];
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Account  $account
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Account $account)
+    {
+        return $account;
+    }
+
+}
