591950f034c3bbf7c8dc1a72123e06676f409aba
Ben Dechrai <ben@dechrai.com>
2017-05-29 16:06:30 +1000

    Change to a password that doesn't look like the field name - better for debugging

--- profile.dev/database/seeds/UserSeeder.php
+++ profile.dev/database/seeds/UserSeeder.php
@@ -14,7 +14,7 @@ class UserSeeder extends Seeder
         \App\User::create([
             'name' => 'Ben Dechrai',
             'email' => 'ben@dechrai.com',
-            'password' => bcrypt('$2y$10$bep9dechrai.cop9cihgmOrQ9v5bpyUYFT2kKL4aNS9pnyqJyBiWK'),
+            'password' => bcrypt('$2y$10$bep9dechrai.cop9cihgmOQTcOdszUrik/ibIsnmDwO9aiOI0YBHO'), // letmein
             'remember_token' => str_random(10),
         ]);
     }
