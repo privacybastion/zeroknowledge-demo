40a94390dbc01aa3ce8c5ace5c3dfaf0799cd6d5
Ben Dechrai <ben@dechrai.com>
2017-05-31 18:33:32 +1000

    Don't require token for OPTIONS requests. Bearer token can be PSK for profile server access, or a valid Token for Browser access.

--- transaction/app/Http/Middleware/BearerToken.php
+++ transaction/app/Http/Middleware/BearerToken.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use App\Token;
 
 class BearerToken
 {
@@ -15,12 +16,24 @@ class BearerToken
      */
     public function handle($request, Closure $next)
     {
+        // OPTIONS requests are always allowed
+        if(\strtolower($request->getMethod()) == "options") {
+            return $next($request);
+        }
+
+        // Anything else must have an auth bearer
         if ($request->hasHeader('Authorization')) {
             list($authScheme, $authValue) = explode(' ', $request->header('Authorization'), 2);
+
             if ($authScheme == 'Bearer' && $authValue == 'secret_token') {
                 return $next($request);
             }
+
+            if($token = Token::where('token', $authValue)->first()) {
+                return $next($request);
+            }
         }
+
         throw new \Exception("NOT FOR YOUR EYES, NSA");
     }
 }
