023374e075980b94d27dedcd34a7987a5c20bd4b
Ben Dechrai <ben@dechrai.com>
2017-05-31 18:34:20 +1000

    Compiled assets

--- profile/public/js/app.js
+++ profile/public/js/app.js
@@ -38279,61 +38279,83 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 $(document).ready(function () {
 
-    // Some form elements are trimmed on change
-    $('input#email').change(function (e) {
-        e.target.value = e.target.value.trim();
-    });
+    function PB_Profile() {}
 
-    // On login submit, hash password
-    $('form#login').submit(function (e) {
+    PB_Profile.user_data = {};
 
-        // Salt is 22 chars long. Use email address and pad if required
-        var salt = ($('#email', e.target).val() + "#cihgmalgbakneismcoidf").substring(0, 22);
-        salt = "$2y$10$" + salt;
+    PB_Profile.init = function () {
 
-        // Grab password, save to session storage, then blank the input field
-        var password = $('#password', e.target).val();
-        sessionStorage.setItem('password', password);
-        console.log("Saved " + password + " in sessionStorage");
-        $('#password', e.target).val('');
+        var $this = this;
 
-        // Crypt and set bcrypt field
-        var bcrypt = __webpack_require__(112);
-        var hashPassword = bcrypt.hashSync(password, salt);
-        console.log("Hashed password " + password + " to " + hashPassword + "");
-        $('#bcrypt', e.target).val(hashPassword);
-    });
+        // On login submit, hash password
+        $("form#login").submit(function (e) {
 
-    $.get('/api/user').done(function (user_data) {
-        checkPublicKey(user_data);
-    });
+            // Grab password, save to session storage, then blank the input field
+            var password = $("#password", e.target).val();
+            console.log('Got password ' + password);
+            sessionStorage.setItem("password", password);
+            $("#password", e.target).val("");
 
-    function checkPublicKey(user_data) {
+            // Salt is 22 chars long. Use email address and pad if required
+            var salt = ($("#email", e.target).val() + "#cihgmalgbakneismcoidf").substring(0, 22);
+            salt = "$2y$10$" + salt;
+
+            // Crypt and set bcrypt field
+            var bcrypt = __webpack_require__(112);
+            var hashPassword = bcrypt.hashSync(password, salt);
+            $("#bcrypt", e.target).val(hashPassword);
+        });
+
+        // Get this user's profile data and check it has a public key (new users don't).
+        $.ajax({
+            url: "/api/user",
+            type: "GET",
+            context: $this
+
+        }).done(function (user_data) {
+            $this.user_data = user_data;
+            $this.checkPublicKey();
+        });
+    };
+
+    PB_Profile.checkPublicKey = function () {
+        var $this = this;
 
         // Does the user have a profile keypair yet?
-        if (user_data.profile_private_key == '') {
+        if ($this.user_data.profile_private_key == "") {
 
             // No keypair - create one using the user's login password
-            console.log("Create profile keypair with password:", sessionStorage.getItem('password'));
-            asyncCreateKeyPair({ passphrase: sessionStorage.getItem('password') }).done(function (profile_key) {
+            $this.asyncCreateKeyPair({
+                passphrase: sessionStorage.getItem("password")
+            }).done(function (profile_key) {
 
                 // Save new keypair to user profile
-                $.post('/api/user', {
-                    profile_private_key: profile_key.private_key,
-                    profile_public_key: profile_key.public_key
+                $.ajax({
+                    url: "/api/user",
+                    type: "POST",
+                    data: {
+                        profile_private_key: profile_key.private_key,
+                        profile_public_key: profile_key.public_key
+                    },
+                    dataType: "json",
+                    context: $this
+
                 }).done(function (user_data) {
-                    checkPrivateData(user_data);
+                    $this.user_data = user_data;
+                    $this.checkPrivateData();
                 });
             });
         } else {
-            checkPrivateData(user_data);
+            $this.checkPrivateData();
         }
-    }
+    };
 
-    function checkPrivateData(user_data) {
+    PB_Profile.checkPrivateData = function () {
+
+        var $this = this;
 
         // Does the user have profile_data yet?
-        if (user_data.profile_data == '') {
+        if ($this.user_data.profile_data == "") {
 
             // User needs profile data, which comprises a keypair for transaction data
 
@@ -38341,73 +38363,131 @@ $(document).ready(function () {
             var promptText = "Please provide a secure passphrase for your transaction data.\n" + "\n" + "This must be different to the password used to log in to your profile.";
 
             // Create the keypair for transaction data
-            asyncCreateKeyPair({ promptText: promptText }).done(function (transaction_key) {
+            $this.asyncCreateKeyPair({
+                promptText: promptText
+            }).done(function (transaction_key) {
 
                 // Encrypt the data using the profile key
                 var profile_data = {
-                    account_references: [1, 3, 5],
+                    account_references: [],
                     transaction_private_key: transaction_key.private_key,
                     transaction_public_key: transaction_key.public_key
                 };
 
-                asyncEncrypt(profile_data, user_data.profile_public_key).done(function (ciphertext) {
+                $this.asyncEncrypt(profile_data, $this.user_data.profile_public_key).done(function (ciphertext) {
 
                     // Save the encrypted profile data to the user profile
-                    $.post('/api/user', {
-                        profile_data: ciphertext.data
+                    $.ajax({
+                        url: "/api/user",
+                        type: "POST",
+                        data: {
+                            profile_data: ciphertext.data
+                        },
+                        dataType: "json",
+                        context: $this
+
                     }).done(function (user_data) {
-                        startApp(user_data);
+                        $this.user_data = user_data;
+                        $this.startApp();
                     });
                 });
             });
         } else {
-            startApp(user_data);
+            $this.startApp();
         }
-    }
+    };
 
-    function startApp(user_data) {
+    PB_Profile.startApp = function () {
+
+        var $this = this;
 
         // Decrypt the profile data
         var openpgp = __webpack_require__(34);
-        var privKeyObj = openpgp.key.readArmored(user_data.profile_private_key).keys[0];
-        privKeyObj.decrypt(sessionStorage.getItem('password'));
+        var privKeyObj = openpgp.key.readArmored($this.user_data.profile_private_key).keys[0];
+        privKeyObj.decrypt(sessionStorage.getItem("password"));
         var options = {
-            message: openpgp.message.readArmored(user_data.profile_data),
-            publicKeys: openpgp.key.readArmored(user_data.profile_public_key).keys,
+            message: openpgp.message.readArmored($this.user_data.profile_data),
+            publicKeys: openpgp.key.readArmored($this.user_data.profile_public_key).keys,
             privateKey: privKeyObj
         };
         openpgp.decrypt(options).then(function (plaintext) {
+            $this.user_data.profile_data = JSON.parse(plaintext.data);
+            $this.renderAccounts();
+        });
+    };
 
-            var data = JSON.parse(plaintext.data);
-            console.log("App started. Encrypted data:");
-            console.log(data);
+    PB_Profile.renderAccounts = function () {
 
-            $('.panel').parent().append('<div class="panel panel-default" id="js-panel"><div class="panel-heading">Accounts</div><div class="panel-body"><p>Loading Account Information</p></div></div>');
+        var $this = this;
 
-            var account_references = data.account_references;
+        $("#js-panel .panel-body").html("");
+        for (var account_ptr = 0; account_ptr < $this.user_data.profile_data.account_references.length; account_ptr++) {
+            $("#js-panel .panel-body").append("<p>" + $this.user_data.profile_data.account_references[account_ptr] + "</p>");
+        }
+        $("#js-panel .panel-body").append("<hr/>");
+        $("#js-panel .panel-body").append('<button id="new-account">Create new account</button>');
 
-            $('#js-panel .panel-body').html('');
-            for (var account_ptr = 0; account_ptr < account_references.length; account_ptr++) {
-                $('#js-panel .panel-body').append('<p>' + account_references[account_ptr] + '</p>');
-            }
+        $("#new-account").click(function (e) {
 
-            $('#js-panel .panel-body').append('<hr/>');
-            $('#js-panel .panel-body').append('<button id="new-account">Crate new account</button>');
+            $("#js-panel .panel-body").html("<p>Loading Account Information</p>");
 
-            $('#new-account').click(function (e) {
-                $.post('http://transaction.dev/api/v1/accounts').done(function (a) {
-                    console.log(a);
-                });
+            $.ajax({
+                url: "http://transaction.example/api/v1/accounts",
+                type: "POST",
+                crossDomain: true,
+                data: {
+                    public_key: $this.user_data.profile_data.transaction_public_key
+                },
+                dataType: "json",
+                xhrFields: {
+                    withCredentials: true
+                },
+                beforeSend: function beforeSend(request) {
+                    request.setRequestHeader("Authorization", "Bearer " + window.PrivacyBastion.transactionToken);
+                },
+                context: $this
+
+            }).done(function (account) {
+
+                $this = this;
+
+                if (account.hasOwnProperty('id') && account.id != null) {
+
+                    // Add this new account number to the list of accounts in the profile data
+                    var profile_data = $this.user_data.profile_data;
+                    profile_data.account_references.push(account.id);
+                    console.log(profile_data);
+
+                    $this.asyncEncrypt(profile_data, $this.user_data.profile_public_key).done(function (ciphertext) {
+
+                        // Save the encrypted profile data to the user profile
+                        $.ajax({
+                            url: "/api/user",
+                            type: "POST",
+                            data: {
+                                profile_data: ciphertext.data
+                            },
+                            dataType: "json",
+                            context: $this
+
+                        }).done(function (user_data) {
+                            $this = this;
+
+                            $this.user_data = user_data;
+                            $this.startApp();
+                        });
+                    });
+                }
             });
         });
-    }
+    };
 
-    function asyncCreateKeyPair(options) {
+    PB_Profile.asyncCreateKeyPair = function (options) {
         var d = $.Deferred();
 
         // Get or use passphrase
         var passphrase;
-        if (options.passphrase && options.passphrase != '') {
+        if (options.passphrase && options.passphrase != "") {
             passphrase = options.passphrase;
         } else {
             var promptText = options.promptText || "Please provide a password";
@@ -38416,7 +38496,7 @@ $(document).ready(function () {
 
         // Build key options
         var options = {
-            userIds: [{ name: 'Privacy Bastion', email: 'privacy.bastion@example.com' }],
+            userIds: [{ name: "Privacy Bastion", email: "privacy.bastion@example.com" }],
             numBits: 1024,
             passphrase: passphrase
         };
@@ -38430,9 +38510,9 @@ $(document).ready(function () {
             });
         });
         return d.promise();
-    }
+    };
 
-    function asyncEncrypt(data, public_key) {
+    PB_Profile.asyncEncrypt = function (data, public_key) {
         var d = $.Deferred();
         var openpgp = __webpack_require__(34);
         var options = {
@@ -38443,7 +38523,11 @@ $(document).ready(function () {
             d.resolve(ciphertext);
         });
         return d.promise();
-    }
+    };
+
+    $(".panel").parent().append('<div class="panel panel-default" id="js-panel">' + '<div class="panel-heading">Accounts</div>' + '<div class="panel-body"></div>' + '</div>');
+
+    PB_Profile.init();
 });
 
 /***/ }),
@@ -78802,7 +78886,7 @@ var Component = __webpack_require__(183)(
   /* cssModules */
   null
 )
-Component.options.__file = "/home/ben/Projects/MooBox/profile.dev/resources/assets/js/components/Example.vue"
+Component.options.__file = "/home/ben/Projects/ZeroKnowledge/profile/resources/assets/js/components/Example.vue"
 if (Component.esModule && Object.keys(Component.esModule).some(function (key) {return key !== "default" && key !== "__esModule"})) {console.error("named exports are not supported in *.vue files.")}
 if (Component.options.functional) {console.error("[vue-loader] Example.vue: functional components are not supported with templates, they should use render functions.")}
 
@@ -78813,9 +78897,9 @@ if (false) {(function () {
   if (!hotAPI.compatible) return
   module.hot.accept()
   if (!module.hot.data) {
-    hotAPI.createRecord("data-v-74211ebc", Component.options)
+    hotAPI.createRecord("data-v-d91362ae", Component.options)
   } else {
-    hotAPI.reload("data-v-74211ebc", Component.options)
+    hotAPI.reload("data-v-d91362ae", Component.options)
   }
 })()}
 
@@ -78904,7 +78988,7 @@ module.exports.render._withStripped = true
 if (false) {
   module.hot.accept()
   if (module.hot.data) {
-     require("vue-hot-reload-api").rerender("data-v-74211ebc", module.exports)
+     require("vue-hot-reload-api").rerender("data-v-d91362ae", module.exports)
   }
 }
 
