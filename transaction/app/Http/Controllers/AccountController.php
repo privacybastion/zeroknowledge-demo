239c3340eb4581bfee967937f0fc223aafc92ef4
Ben Dechrai <ben@dechrai.com>
2017-05-31 18:33:42 +1000

    Fix typo

--- transaction/app/Http/Controllers/AccountController.php
+++ transaction/app/Http/Controllers/AccountController.php
@@ -10,18 +10,18 @@ class AccountController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        return Account::create[$request->all()];
+        return Account::create($request->all());
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Account  $account
+     * @param  \App\Account $account
      * @return \Illuminate\Http\Response
      */
     public function show(Account $account)
