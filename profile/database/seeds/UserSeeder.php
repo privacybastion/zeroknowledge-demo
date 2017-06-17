106d88414b469ca55ee44050a51fd3cec5fe2dbf
Ben Dechrai <ben@dechrai.com>
2017-06-17 10:43:27 +1000

    Remove default user

--- profile/database/seeds/UserSeeder.php
+++ profile/database/seeds/UserSeeder.php
@@ -11,11 +11,5 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        \App\User::create([
-            'name' => 'Ben Dechrai',
-            'email' => 'ben@dechrai.com',
-            'password' => bcrypt('$2y$10$bep9dechrai.cop9cihgmOQTcOdszUrik/ibIsnmDwO9aiOI0YBHO'), // letmein
-            'remember_token' => str_random(10),
-        ]);
     }
 }
